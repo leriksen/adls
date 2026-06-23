ef96b21c95381c0b9a4501d8cf9af79d1484cb9c
Leif Eriksen <leif.eriksen.au@gmail.com>
2026-06-23 20:53:52 +1000

    dont deploy automation if not in a lower env

--- terraform/automation.tf
+++ terraform/automation.tf
@@ -1,4 +1,13 @@
+locals {
+  automation_enabled = contains(["dev", "tst"], var.environment)
+  sftp_automation_map = local.automation_enabled ? {
+    for k, v in local.storage_map : k => v if try(v.sftp_enabled, false)
+  } : {}
+}
+
 resource "azurerm_automation_account" "aa" {
+  for_each = local.automation_enabled ? { aa = {} } : {}
+
   name                = "${azurerm_resource_group.arg.name}aa"
   location            = module.global.location
   resource_group_name = azurerm_resource_group.arg.name
@@ -11,20 +20,20 @@ resource "azurerm_automation_account" "aa" {
 }
 
 resource "azurerm_role_assignment" "aa_sa_contributor" {
-  for_each = { for k, v in local.storage_map : k => v if try(v.sftp_enabled, false) }
+  for_each = local.sftp_automation_map
 
-  principal_id         = azurerm_automation_account.aa.identity[0].principal_id
+  principal_id         = azurerm_automation_account.aa["aa"].identity[0].principal_id
   role_definition_name = "Storage Account Contributor"
   scope                = module.sa[each.key].id
 }
 
 resource "azurerm_automation_runbook" "sftp_toggle" {
-  for_each = { for k, v in local.storage_map : k => v if try(v.sftp_enabled, false) }
+  for_each = local.sftp_automation_map
 
   name                    = "${azurerm_resource_group.arg.name}dl${each.key}_sftp_toggle"
   location                = module.global.location
   resource_group_name     = azurerm_resource_group.arg.name
-  automation_account_name = azurerm_automation_account.aa.name
+  automation_account_name = azurerm_automation_account.aa["aa"].name
   log_verbose             = false
   log_progress            = false
   runbook_type            = "PowerShell"
